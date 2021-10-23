d532698bdc2787f55572796b182341f636565b29
pandresvaldivia <pandresvaldiviacuzcano@gmail.com>
2021-10-23 14:22:51 -0500

    Menu component created

--- js/functions.js
+++ js/functions.js
@@ -1,15 +1,16 @@
-import { $mobileMenu } from './selectors.js';
+import { $mobileMenu, $settingsMenu } from './selectors.js';
 
 function handleBurguerMenu(button) {
-	console.log(button);
 	const isPressed = button.classList.toggle('is-pressed');
 	if (isPressed) {
 		$mobileMenu.classList.add('is-expanded');
+		$settingsMenu.classList.remove('is-hidden');
 		button.ariaPressed = 'true';
 		button.ariaExpanded = 'true';
 		button.ariaLabel = 'Colapsar menu';
 	} else {
 		$mobileMenu.classList.remove('is-expanded');
+		$settingsMenu.classList.add('is-hidden');
 		button.ariaPressed = 'false';
 		button.ariaExpanded = 'false';
 		button.ariaLabel = 'Desplegar menu';
--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,3 @@
 export const $burgerBtn = document.querySelector('.burgerButton');
 export const $mobileMenu = document.querySelector('.mobileMenu');
+export const $settingsMenu = document.querySelector('#settings-menu');
